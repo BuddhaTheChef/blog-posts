d09668c4273093f38479827feeb8ac2065d8d527
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-07-01 13:19:38 -1000

    connecting routes and middleware

--- src/index.js
+++ src/index.js
@@ -3,28 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 import './App.css';
 
-import App from './components/app';
+import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() {return <div>Hello!</div>}
-}
-
-class Goodbye extends React.Component {
-  render() {return <div>Goodbye!</div>}
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <h1 className="header">Header</h1>
-      <Route path="/hello" component={Hello} />
-      <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
